51ac0097f7620bd55e6e208c6ebdc1b321e75a08
David Mesia <davidmesia@Davids-MacBook-Pro.local>
2020-12-04 21:19:33 -0500

    adds new motorcycle functionality

--- controllers/motorcycles.js
+++ controllers/motorcycles.js
@@ -1,11 +1,27 @@
 const Motorcycle = require('../models/motorcycle');
 
 module.exports = {
-    index
+    index,
+    new: newMotorcycle,
+    create
 };
 
 function index(req, res) {
     Motorcycle.find({}, function(err, motorcycles) {
         res.render('motorcycles/index', { motorcycles });
+    });
+};
+
+function newMotorcycle(req, res) {
+    res.render('motorcycles/new');
+};
+
+function create(req, res) {
+    const motorcycle = new Motorcycle(req.body);
+
+    motorcycle.save(function (err) {
+        console.log(motorcycle);
+        if (err) return res.render('motorcycles/new');
+        res.redirect('/motorcycles');
     });
 };
--- routes/motorcycles.js
+++ routes/motorcycles.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const motorcyclesCtrl = require('../controllers/motorcycles');
 
 router.get('/motorcycles', motorcyclesCtrl.index);
+router.get('/motorcycles/new', motorcyclesCtrl.new);
+router.post('/motorcycles', motorcyclesCtrl.create);
 
 module.exports = router;
